fd5006727c5bf3bff04e326f05b56e49b2c308cf
MehrajShakil <54067524+MehrajShakil@users.noreply.github.com>
2026-08-08 22:01:32 +0600

    Add User, Tweet, Like, Retweet, Follow, Notification, Message, Bookmark entities (#1)
    * Add user class
    
    * Add Tweet, Like, Retweet, Follow, Notification, Message, Bookmark entities
    
    ---------
    
    Co-authored-by: = <=>

---
+++ TwitterClone/TwitterClone.Domain/Entities/User.cs
@@ -0,0 +1,41 @@
+﻿namespace TwitterClone.Domain.Entities
+{
+    public class User
+    {
+        private Guid _id;
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        private DateTime _createdAt;
+        private DateTime _modifiedAt;
+
+        public User()
+        {
+            _id = Guid.NewGuid();
+            _createdAt = DateTime.UtcNow;
+        }
+
+        public Guid Id
+        {
+            get {  return _id; }
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public DateTime CreatedAt
+        {
+            get { return _createdAt; }
+        }
+
+        public DateTime ModifiedAt
+        {
+            get { return _modifiedAt; }
+            set { _modifiedAt = value; }
+        }
+
+    }
+}
